bb6fc6d8dad3b02c91df22b3b7cf4f01ef3510b1
dhruva-25 <ds23adi@herts.ac.uk>
2025-05-04 18:45:28 +0100

    email

---
+++ src/mail/email.py
@@ -0,0 +1,40 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import markdown
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+user = os.environ['EMAIL']
+passward = os.environ['EMAIL_KEY']
+
+
+
+def email_sender(destination_city: str, sender_id, result: str):
+    # --- Configuration ---
+    user = user 
+    password = passward 
+    to_email = sender_id
+    subject = f"🧳 Your Travel Itinerary for {destination_city} City."
+    
+    # Convert markdown to HTML
+    html_content = markdown.markdown(result)
+    
+    # --- Build the Email ---
+    msg = MIMEMultipart("alternative")
+    msg['From'] = user
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    # Attach the HTML part
+    msg.attach(MIMEText(html_content, 'html'))
+    
+     # --- Send the Email via Gmail SMTP ---
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+            server.login(user, password)
+            server.sendmail(user, to_email, msg.as_string())
+        print("✅ Email sent successfully.")
+    except Exception as e:
+        print(f"❌ Failed to send email: {e}")
